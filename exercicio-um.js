567d070739379f02258956041d26b9db6787e787
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-05 09:20:26 -0300

    função typeof()

---
+++ exercicio-um.js
@@ -0,0 +1,11 @@
+/*Declare três variáveis diferentes (uma para cada tipo: string, número e booleano) e atribua valores a elas. 
+Em seguida, exiba o tipo de cada variável no console.*/
+const text = 'String aqui';
+const number = 5;
+const boolean = true;
+
+console.log(text + " = " + typeof(text));
+console.log(number + " = " + typeof(number));
+console.log(boolean + " = " + typeof(boolean));
+
+/* */
