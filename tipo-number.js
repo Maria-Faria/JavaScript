6e5b43f20ede2c705e30833059a006e053632e7a
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-07 08:54:00 -0300

    number

---
+++ tipo-number.js
@@ -0,0 +1,12 @@
+const notaPrimeiroBi = 8;
+const notaSegundoBi = 6.3;
+const notaTerceiroBi = -2;
+const notaQuartoBi = Number.parseInt('5');
+
+const total = (notaPrimeiroBi + notaSegundoBi + notaTerceiroBi + notaQuartoBi)/4;
+
+console.log('A média é: ' + total.toFixed(2));
+
+const nome = 'Maria';
+
+console.log('Meu nome é ' + nome);
