0e7b4a01c076f36e9bec6541cea5be3f984d9beb
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-07 09:18:17 -0300

    strings

---
+++ tipo-string.js
@@ -0,0 +1,14 @@
+const student = "Maria";
+const teacher = 'Vitória';
+const greeting = "Nosso lema é 'estudar bastante!'";
+const quote = `Dostoievski diz: "O erro é uma coisa boa, porque conduz à verdade"`;
+
+console.log(greeting);
+console.log(quote);
+
+//template string
+console.log(`A estudante chama ${student}`);
+
+const password = 'SenhaSegura123' + student.toUpperCase();
+
+console.log(password);
