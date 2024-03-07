5c55d1b3596dc7b73a4cd0e528ae57e7f3fdb0e2
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-07 09:41:54 -0300

    boolean

---
+++ tipo-boolean.js
@@ -0,0 +1,16 @@
+const student = 'Eduarda';
+const approved = true;
+
+if(approved) {
+    console.log('Parabéns, boas festas!');
+
+}else {
+    console.log('REPROVADO, boas festas!');
+}
+
+if(student === 'Eduarda') {
+    console.log(`Olá, ${student}`);
+
+}else {
+    console.log('Quem é você?')
+}
