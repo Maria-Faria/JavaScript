2cae3ec45e6c1b9fb5cf409f87d51deb4d528b23
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-12 08:47:45 -0300

    null and undefined

---
+++ tipo-null-undefined.js
@@ -0,0 +1,10 @@
+let studentName;
+const teacher = 'Ana';
+
+console.log(studentName);
+console.log(typeof(studentName));
+console.log(typeof(teacher));
+
+let studentPhone = null;
+
+console.log(typeof(studentPhone));
