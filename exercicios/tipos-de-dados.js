24eeab83c6de06d32ac9b8450516e047ca785ecd
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-12 09:17:39 -0300

    exercises

---
+++ exercicios/tipos-de-dados.js
@@ -0,0 +1,10 @@
+/*Crie uma variável contendo uma frase. Em seguida, exiba no console o comprimento da frase e a mesma frase em constras maiúsculas.*/ 
+
+//criando a variável com a frase
+const phrase = 'Just give me a reason';
+
+//exibindo o comprimento da frase
+console.log('Comprimento da frase (com espaços): ' + phrase.length);
+
+//frase em capslock
+console.log(phrase.toUpperCase());
