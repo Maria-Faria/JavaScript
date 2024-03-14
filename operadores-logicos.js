271869cc828426572ba30d50283de8be30658add
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-14 09:41:30 -0300

    operadores lógicos - not

--- operadores-logicos.js
+++ operadores-logicos.js
@@ -1,9 +1,17 @@
 const notaFinal = 6;
-const faltas = 5;
+const faltas = 2;
+const advertencias = 0;
 
 if (notaFinal < 7 && faltas > 4) {
     console.log('Reprovado :(');
 
 }else {
     console.log('Não foi reprovado por falta');
+}
+
+if(faltas <= 2 && !advertencias) {
+    console.log('Recebeu bônus');
+
+}else {
+    console.log('Não recebeu bônus');
 }
