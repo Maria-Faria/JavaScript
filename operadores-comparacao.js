ed4c4357c4b4cc28c80f2342e7142c3970f5d449
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-15 09:23:29 -0300

    operadores de comparação

---
+++ operadores-comparacao.js
@@ -0,0 +1,22 @@
+const estaAprovado = true;
+
+if(estaAprovado === true) {
+    console.log('Aprovado');
+}
+
+if('0' === 0) {
+    console.log('Passou na comparação');
+
+}else {
+    console.log('Não passou na comparação');
+}
+
+const idadeMinima = 18;
+const idadeEstudante = 16;
+
+if(idadeEstudante >= idadeMinima) {
+    console.log('Não precisa de autorização');
+
+}else {
+    console.log('Precisa de autorização');
+}
