3442279b84007ce0294cde2723b07f58d7033474
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-16 18:29:58 -0300

    exercício 3 - palíndromos

--- exercicios/funcoes.js
+++ exercicios/funcoes.js
@@ -27,4 +27,20 @@ const isAdult = function(age) {
 console.log(isAdult(12));
 line();
 
-/* */
+/*Crie uma função que receba uma string e verifique se é um palíndromo 
+(uma palavra que é lida da mesma forma de trás para frente) utilizando o método de string reverse(). 
+Retorne true se for um palíndromo e false caso contrário. Imprima o resultado no console.*/
+
+function isPalindromo(word) {
+    const letters = word.split('');
+    const reverseWord = letters.reverse().join('');
+    
+    if(word === reverseWord) {
+        return true;
+    
+    }else {
+        return false;
+    }
+}
+
+console.log(isPalindromo('arara'));
