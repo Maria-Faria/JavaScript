ba6c460d4c5f9c0541547151a83156ed2bbbb5d6
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-16 18:17:12 -0300

    exercicio 2 - verificando se uma pessoa é maior de idade

--- exercicios/funcoes.js
+++ exercicios/funcoes.js
@@ -1,3 +1,7 @@
+function line() {
+    console.log('---------------------------------------');
+}
+
 /*Crie uma função que receba o nome de uma pessoa como argumento e retorne uma saudação personalizada. 
 Em seguida, chame a função e exiba a saudação no console.*/
 
@@ -6,3 +10,21 @@ function greeting(name) {
 }
 
 console.log(greeting('Kate'));
+line();
+
+/*Crie uma função que receba a idade de uma pessoa e retorne se ela é maior de idade (idade >= 18). 
+Imprima o resultado no console.*/
+
+const isAdult = function(age) {
+    if(age >= 18) {
+        return 'Você é maior de idade';
+
+    }else {
+        return 'Você não é maior de idade';
+    }
+}
+
+console.log(isAdult(12));
+line();
+
+/* */
