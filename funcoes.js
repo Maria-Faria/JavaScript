e225693b17839cc0b7aca393bc9d350d5e08237f
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-16 17:28:24 -0300

    functions

---
+++ funcoes.js
@@ -0,0 +1,9 @@
+//parâmetros/argumentos
+//retorno
+
+function showStudentName(name, grade) {
+    return `O nome é ${name} e a nota é ${grade}`;
+}
+
+console.log(showStudentName('Caroline', 10));
+console.log(showStudentName('Júlio', 5));
