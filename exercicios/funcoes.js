481d7709ddcd038df877cb9ca8602ae36600362a
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-16 18:11:41 -0300

    exercicio 1 - saudação personalizada

---
+++ exercicios/funcoes.js
@@ -0,0 +1,8 @@
+/*Crie uma função que receba o nome de uma pessoa como argumento e retorne uma saudação personalizada. 
+Em seguida, chame a função e exiba a saudação no console.*/
+
+function greeting(name) {
+    return `Hello, ${name}!`;
+}
+
+console.log(greeting('Kate'));
