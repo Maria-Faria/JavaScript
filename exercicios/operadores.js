db9583ba3d3cda263f4390cc5a82d23de0028230
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-15 23:56:20 -0300

    condicionais

--- exercicios/operadores.js
+++ exercicios/operadores.js
@@ -55,3 +55,19 @@ if(var1 || var2) {
 }else {
     console.log('Nenhuma das variáveis é verdadeira');
 }
+
+console.log('-----------------------------------------');
+
+/*Imagine um sistema que determina se um usuário tem idade suficiente para comprar o ingresso para um show. 
+Declare duas variáveis que determinem a idade mínima e qual a idade do usuário e utilize estruturas condicionais (if, else) 
+para determinar se o usuário pode realizar a compra.*/
+
+const idadeMinima = 18;
+const idadeUsuario = 20;
+
+if(idadeUsuario >= idadeMinima) {
+    console.log('Pode comprar o ingresso para o show');
+
+}else {
+    console.log('Não pode comprar o ingresso para o show');
+}
