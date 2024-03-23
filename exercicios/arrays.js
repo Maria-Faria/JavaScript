434335ebe4dd0d697fa19b088b1652b1e874b43a
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-23 14:20:09 -0300

    modificando arrays

--- exercicios/arrays.js
+++ exercicios/arrays.js
@@ -21,4 +21,15 @@ array.unshift(0);
 console.log(`Array modificado: ${array}`);
 line();
 
-/* */
+/*Crie um array vazio chamado meuArray e adicione 3 números inteiros de sua escolha utilizando o método push(). 
+Imprima no console os itens presentes no array para verificar se os números foram adicionados. 
+Em seguida, substitua o primeiro elemento do array (índice 0) pelo dobro do seu valor atual. 
+Imprima no console o array atualizado para verificar a mudança.*/
+const meuArray = [];
+
+meuArray.push(17, 18, 19);
+console.log(meuArray);
+
+meuArray[0] = meuArray[0] * 2;
+console.log(meuArray);
+line();
