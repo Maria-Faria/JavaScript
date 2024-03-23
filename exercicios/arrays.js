652554a32ce2c19ddf3f48dff6b7389d44975a9c
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-23 14:05:01 -0300

    arrays esparsos

---
+++ exercicios/arrays.js
@@ -0,0 +1,8 @@
+/*Um array esparso é um tipo de array no qual a maioria dos elementos tem valores indefinidos. 
+Crie um array esparso e atribua valores a alguns elementos do array (por exemplo, defina valores 
+nos índices 1, 3 e 7). Imprima no console o array e também a informação de comprimento do array.*/
+
+const arrayEspars = [, 10,, 15,,,,20,,,];
+
+console.log(arrayEspars);
+console.log(`Comprimento do array: ${arrayEspars.length}`);
