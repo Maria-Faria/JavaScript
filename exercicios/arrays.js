88b3ec24a8aefa0f0e2b2ecef780f9b1328e491b
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-23 14:31:37 -0300

    método pop

--- exercicios/arrays.js
+++ exercicios/arrays.js
@@ -42,3 +42,20 @@ console.log(emptyArray);
 emptyArray.push(54, 25, 10);
 console.log(emptyArray);
 line();
+
+/*Desenvolva um programa em Node.js que simule uma clínica veterinária. Crie um array vazio chamado clinica que representará a 
+fila de animais na clínica. Em um primeiro momento, simule a chegada de três animais diferentes e exiba a lista de animais no console. 
+Após a exibição, remova os animais da lista um por vez e, por fim, exiba no console o estado final da lista.*/
+const clinica = [];
+
+clinica.push('cachorro', 'gato', 'peixe');
+console.log(clinica);
+
+clinica.pop();
+console.log(clinica);
+
+clinica.pop();
+console.log(clinica);
+
+clinica.pop();
+console.log(clinica);
