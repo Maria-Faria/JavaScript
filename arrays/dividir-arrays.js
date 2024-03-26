042178afd6ca6b2cf0076391e8e8c868bb16433d
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-26 09:31:56 -0300

    atualizando arrays

---
+++ arrays/dividir-arrays.js
@@ -0,0 +1,8 @@
+const listaEstudantes = ['João', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme', 'Aline', 'Fabiana', 'Andre', 
+'Carlos', 'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinicius', 'Renan', 'Renata', 'Daisy', 'Camila'];
+
+const sala1 = listaEstudantes.slice(0, listaEstudantes.length/2);
+const sala2 = listaEstudantes.slice(listaEstudantes.length/2);
+
+console.log(sala1);
+console.log(sala2);
