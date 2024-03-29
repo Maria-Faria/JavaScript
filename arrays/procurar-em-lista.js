443ed385021710875bf1b44eeec8c48c25f96925
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-29 15:58:42 -0300

    desestruturando uma lista

--- arrays/procurar-em-lista.js
+++ arrays/procurar-em-lista.js
@@ -5,8 +5,10 @@ const lista = [alunos, medias];
 
 function exibeNomeENota(aluno) {
     if(lista[0].includes(aluno)) {
-        const indice = lista[0].indexOf(aluno);
-        const mediaAluno = lista[1][indice];
+        const [alunos, medias]  = lista;
+
+        const indice = alunos.indexOf(aluno);
+        const mediaAluno = medias[indice];
         console.log(`${aluno} tem a nota ${mediaAluno}.`);
     
     }else {
