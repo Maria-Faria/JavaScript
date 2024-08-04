7eedcd3f1f73e29c0de01caa3b303b9fe8f807b3
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-08-03 22:30:12 -0300

    adicionando e alterando informações

---
+++ objetos/alterando-objeto.js
@@ -0,0 +1,14 @@
+const student = {
+    name: 'José Silva',
+    age: 32,
+    cpf: '45671258932',
+    class: 'JavaScript'
+};
+
+student.phone = '12996702149';
+
+console.log(student.phone);
+console.log(student);
+
+student.name = 'José Souza';
+console.log(student);
