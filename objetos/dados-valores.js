1061df91b8cc98948fc3d3723e7060bbaf79b332
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-08-04 19:20:56 -0300

    tipos de dados e valores / objetos em objetos

---
+++ objetos/dados-valores.js
@@ -0,0 +1,10 @@
+const student = {
+    name: 'José Silva',
+    age: 32,
+    cpf: '45671258932',
+    class: 'JavaScript',
+    colleger: true,
+    phones: ['12996702149', '12885647123']
+};
+
+console.log(student.phones[0]);
---
+++ objetos/objs-aninhados.js
@@ -0,0 +1,16 @@
+const student = {
+    name: 'José Silva',
+    age: 32,
+    cpf: '45671258932',
+    class: 'JavaScript',
+    colleger: true,
+    phones: ['12996702149', '12885647123']
+};
+
+student.address = {
+    street: 'Joaquim Antonio da Rocha',
+    number: '222',
+    complement: 'Logo ali'
+}
+
+console.log(student.address.street);
