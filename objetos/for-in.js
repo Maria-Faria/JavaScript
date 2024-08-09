a2f16da85cd34b583cf8e57e5a2f48909acb5db3
Maria-Faria <mariaeduardadefaria15@gmail.com>
2024-08-09 19:29:32 -0300

    for in

---
+++ objetos/for-in.js
@@ -0,0 +1,19 @@
+const student = {
+    name: 'José Silva',
+    age: 32,
+    cpf: '45671258932',
+    class: 'JavaScript',
+    colleger: true,
+    phones: ['12996702149', '12885647123'],
+    address: [{
+        street: 'Joaquim Antonio da Rocha',
+        number: '222',
+        complement: 'Logo ali'
+    }, 
+    {
+        street: 'Rua Dona Clotilde',
+        number: '71',
+        complement: null
+    }]
+};
+
