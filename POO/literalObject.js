baefbceec7abe9c3e69dcaaf1ae36a2f56e4c342
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-08-17 23:08:52 -0300

    OO with JavaScript

---
+++ POO/literalObject.js
@@ -0,0 +1,11 @@
+const user = {
+    name: "Maria",
+    email: "maria@gmail.com",
+    birth: "15/04/2004",
+    role: "Estudante",
+    active: true,
+    showInfos: function() {
+        console.log(this.name);
+        console.log(this.email);
+    }
+}
