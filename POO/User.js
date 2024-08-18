f3e8340dfd6b153a7ca9ec817e8f6b166fc84511
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-08-18 14:18:50 -0300

    class structure

---
+++ POO/User.js
@@ -0,0 +1,18 @@
+class User {
+    constructor(name, email, birth, role, active = true) {
+        this.name = name;
+        this.email = email;
+        this.birth = birth;
+        this.role = role || "estudante";
+        this.active = active;
+    }
+
+    showInfos() {
+        return `Nome: ${this.name}\nEmail: ${this.email}`;
+    }
+}
+
+const user = new User("Maria Eduarda", "maria@gmail.com", "15/04/2004");
+
+console.log(user);
+console.log(user.showInfos());
